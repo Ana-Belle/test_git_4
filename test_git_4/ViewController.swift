56766565694a01ead1c4d49ec84be990a1f9a9d4
Anastasia Belyakova <ana-belle@mail.ru>
2021-10-18 18:29:55 +0300

    Change3

--- test_git_4/ViewController.swift
+++ test_git_4/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Change1")
         print("Change2")
+        print("Change3")
         // Do any additional setup after loading the view.
     }
 
